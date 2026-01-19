d37473809f3b511e5d054f80feeb58961a38a106
KAKZBI <enelibrice@yahoo.fr>
2026-01-19 06:52:50 +0100

    Changed variable aGame to a_game

--- main.rb
+++ main.rb
@@ -9,35 +9,35 @@ def playGame
     print "\nWhat is the name of player #2>> "
     name = gets.chomp
     player_2 = Player.new(name, "O") # player2 will use O
-    aGame = Game.new # Create a new game
-    puts aGame
-    while(!aGame.endGame? && !aGame.has_won?(player_1.letter) && !aGame.has_won?(player_2.letter))
+    a_game = Game.new # Create a new game
+    puts a_game
+    while(!a_game.endGame? && !a_game.has_won?(player_1.letter) && !a_game.has_won?(player_2.letter))
         print "\n#{player_1.name}, please choose a valid number>> "
         choosen_number = gets.chomp.to_i
-        while(!aGame.valid_turn?(choosen_number, player_1.letter))
+        while(!a_game.valid_turn?(choosen_number, player_1.letter))
             puts "\e[1;31m Sorry, this number is invalid \e[0m"
             print "\n#{player_1.name}, please choose a valid number>> "
             choosen_number = gets.chomp.to_i
         end
-        puts aGame
-        break if aGame.has_won?(player_1.letter) || aGame.endGame?
+        puts a_game
+        break if a_game.has_won?(player_1.letter) || a_game.endGame?
         print "\n#{player_2.name}, please choose a valid number>> "
         choosen_number = gets.chomp.to_i
-        while(!aGame.valid_turn?(choosen_number, player_2.letter))
+        while(!a_game.valid_turn?(choosen_number, player_2.letter))
             puts "\033[1;31m Sorry, this number is invalid \033[0m"
             print "\n#{player_2.name}, please choose a valid number>> "
             choosen_number = gets.chomp.to_i
         end
-        puts aGame
-        # break if aGame.has_won?(player_2.letter)
-        # puts " Game end? #{aGame.endGame?}"
+        puts a_game
+        # break if a_game.has_won?(player_2.letter)
+        # puts " Game end? #{a_game.endGame?}"
         
     end
 
     # Print the result of the game
-    if aGame.has_won?(player_1.letter) 
+    if a_game.has_won?(player_1.letter) 
         puts "\e[;32m Match end. #{player_1.name} is the winner\033[0m"
-    elsif aGame.has_won?(player_2.letter)
+    elsif a_game.has_won?(player_2.letter)
         puts "\e[;34m Match end. #{player_2.name} is the winner\033[0m"
     else
         puts "\e[;33m Match end. DRAW\033[0m"
